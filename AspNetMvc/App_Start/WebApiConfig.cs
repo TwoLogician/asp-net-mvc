29647bb6bfcbe01c9e749307747acb9d5d658bcf
Two <Two.Logician@icloud.com>
2021-09-16 16:15:52 +0700

    :tada:

---
+++ AspNetMvc/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace AspNetMvc
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SerializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            };
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AspNetMvc/Controllers/Api/FileController.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace AspNetMvc.Controllers.Api
+{
+    [RoutePrefix("api/File")]
+    public class FileController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post()
+        {
+            var request = HttpContext.Current.Request;
+
+            var directory = HttpContext.Current.Server.MapPath("~/Files");
+            var form = request.Form;
+            var fileNames = new List<string>();
+            var files = request.Files;
+            var json = form.Get("json");
+            var value = JsonConvert.DeserializeObject<dynamic>(json);
+
+            Directory.CreateDirectory(directory);
+
+            for (var i = 0; i < files.Count; i++)
+            {
+                var f = files[0];
+                var path = Path.Combine(directory, f.FileName);
+                f.SaveAs(path);
+                fileNames.Add(f.FileName);
+            }
+
+            return Ok(new
+            {
+                Json = value,
+                Files = fileNames,
+            });
+        }
+    }
+}
